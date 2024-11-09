26a0e852c66bfc6bee1dc91d1a22f955efcaf74e
Euronii <99759785+Euronii@users.noreply.github.com>
2024-11-09 22:31:15 +0100

    Update client.php
    client side

--- src/client.php
+++ src/client.php
@@ -1,15 +1,28 @@
 <?php
-// Server IP and port 
-$server_ip = '';
-$server_port = 5000; 
+// server details
+$server_ip = "localhost";
+$server_port = 1200;
 
-// Create a UDP socket
+// Krijo UDP socket
 $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-if (!$socket) {
-    die("Unable to create socket\n");
+
+while (true) {
+    // Merr mesazh nga perdoruesi
+    echo "Enter message to send to server (or 'exit' to quit): ";
+    $input = trim(fgets(STDIN));
+
+    // Dergon message ne server
+    socket_sendto($socket, $input, strlen($input), 0, $server_ip, $server_port);
+
+    // Qysh do qe e shkrun so case sensitive
+    if (strtolower($input) == "exit") {
+        break;
+    }
+
+    // Merr pergjigjen nga serveri
+    socket_recvfrom($socket, $buffer, 2048, 0, $server_ip, $server_port);
+    echo "Response from server: $buffer\n";
 }
-
-
 
 socket_close($socket);
 ?>
