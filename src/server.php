183f064bd293fd0221a5b57212629179b221ea93
ermiragashi14 <155022915+ermiragashi14@users.noreply.github.com>
2024-11-09 11:44:52 +0000

    Update server.php

--- src/server.php
+++ src/server.php
@@ -1,1 +1,10 @@
+<?php
+//Vendosja e variablave për IP adresën dhe portin
+$server_ip = "0.0.0.0";
+$server_port = 1200;
 
+$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+socket_bind($socket, $server_ip, $server_port);
+echo "Serveri UDP është gati në $server_ip:$server_port\n";
+
+?>
